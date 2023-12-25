1685f7e82574780998e774baee62d26772610e25
Thomas Steinkamp <thomas.steinkamp@home>
2023-12-25 22:38:05 +0100

    - changed dependencies from path to git. - started, compiling, but i had to switch to nightly compiler - moved the contents from blinky.rs to main.rs - after doing rustup target add thumbv6m-none-eabi it compiles (this was proposed by an error message) BUT WHY THE TARGET ADD? I was already installed as the compilation was already working in an other sourcetree... or is this per project?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+//! This example test the RP Pico on board LED.
+//!
+//! It does not work with the RP Pico W board. See wifi_blinky.rs.
+
+#![no_std]
+#![no_main]
+
+use core::time::Duration;
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio;
+use embassy_time::Timer;
+use gpio::{Level, Output};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after_millis(300).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after_millis(300).await;
+    }
 }
